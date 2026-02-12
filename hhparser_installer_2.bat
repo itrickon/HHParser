4d91e9310b598a3d5f95286b584130d75d0b882f
Zakhar Tikhonov <erroraccount@bk.ru>
2026-02-12 14:22:44 +0400

    chromuim-1200/1208

--- hhparser_installer_2.bat
+++ hhparser_installer_2.bat
@@ -26,7 +26,7 @@ pyinstaller --clean --noconfirm ^
 --windowed ^
 --icon="static/HHParse_logo.ico" ^
 --add-data="static;static" ^
---add-data="%LOCALAPPDATA%\ms-playwright\chromium-1200;ms-playwright\chromium-1200" ^
+--add-data="%LOCALAPPDATA%\ms-playwright\chromium-*;ms-playwright" ^
 --runtime-hook=playwright_runtime_hook.py ^
 gui.py
 
